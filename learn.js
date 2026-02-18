fda95564aeb2466b0ff7e8d530fb66d62417b2cb
brana-lee-06 <liruixin016@gmail.com>
2026-02-18 15:20:10 +0700

    Update 1

--- learn.js
+++ learn.js
@@ -1,12 +1,12 @@
 console.log("Hello from JavaScript!");
 //Variables-storing different types of information
 let score=0;      //A number
-let playerName="Somchai"; //A string(text)
+let playerName="Pat"; //A string(text)
 let isPlaying=true;  //A Boolean(true/false)
 // Print them out
 console.log("Player:",playerName);
 console.log("Score:",score);
 console.log("Playing:",isPlaying);
 // Change a variable
-score=10;
+score=20;
 console.log("New score:",score);
